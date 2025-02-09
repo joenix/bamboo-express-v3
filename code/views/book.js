3373d03a87bd9e2866dd7f568c518ba7b122831c
Joenix <joenix.com@gmail.com>
2025-02-09 16:19:01 +0800

    feat: 点读

--- code/views/book.js
+++ code/views/book.js
@@ -222,6 +222,7 @@ async function get_all_exp_by_user(req, res) {
     // 3.
     let exps = [];
     for (const book of books) {
+      console.log(211, book);
       const exp = await get_all_bookhis(1, 10, [{ key: 'book', value: book.id }]);
       console.log(225, exp);
       exp.book = book;
