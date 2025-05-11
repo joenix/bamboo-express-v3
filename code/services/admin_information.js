60a182a3854c55c3543a5dbb5d21712037128ae3
Joenix <joenix.com@gmail.com>
2025-05-11 13:34:54 +0800

    feat: update

--- code/services/admin_information.js
+++ code/services/admin_information.js
@@ -83,8 +83,12 @@ async function get_all(page = 1, pageSize = 10, filters = []) {
     }
   });
 
+  console.log(87, where);
+
   const skip = (page - 1) * pageSize;
   const take = pageSize;
+
+  console.log(92, { skip, take, where });
 
   const data = await prisma.information.findMany({
     skip,
@@ -92,9 +96,13 @@ async function get_all(page = 1, pageSize = 10, filters = []) {
     where
   });
 
+  console.log(98, data);
+
   const counts = await prisma.information.count({
     where: where
   });
+
+  console.log(104, counts);
 
   const totalPages = Math.ceil(counts / pageSize);
 
